eec507a28eb15b86c91e677cb2a170eeaac3012e
KANSEI CHIN <qq1528359634@gmail.com>
2026-01-17 11:20:15 +0900

    Add Readonly class example and update solution file
    Added a new Readonly class in Csharp/MemberModifiers/readnoly.cs to demonstrate the use of the static readonly modifier with arrays. Updated csharp-learning-notes.slnx to include the Csharp project.

---
+++ Csharp/MemberModifiers/readnoly.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Csharp.MemberModifiers
+{
+    internal class Readonly
+    {
+        public static readonly  int[] values={1,2,3};
+       
+        public Readonly()
+        {
+            //values = new[] { 10, 20, 30 };//エラー
+            values[0] = 10;
+        }
+    }
+}
