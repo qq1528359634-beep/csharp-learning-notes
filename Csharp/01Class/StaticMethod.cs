48fce1da09a87b0fa5d72cacd6c59525c88a00d7
KANSEI CHIN <qq1528359634@gmail.com>
2026-01-17 10:59:26 +0900

    Move class files and add static/using examples
    Renamed ClassInheritance files to Csharp directory for better organization. Added StaticMethod.cs to demonstrate static methods and FileOperation/Using.cs to illustrate IDisposable implementation. Updated solution file to reflect project path changes.

---
+++ Csharp/01Class/StaticMethod.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+using System.Text;
+
+namespace Csharp._01Class
+{
+    internal class StaticMethod
+    {
+        private int _number;
+        public static int number;
+       public  static void MyMethod()
+        {
+            number = 1;
+            Console.WriteLine($"MyMethod is used !{number}");
+        }
+    }
+    class program
+    {
+        static void Main()
+        {
+            StaticMethod.MyMethod();  //クラス方法　実例ではなくクラス名で直接訪問  
+        }
+    }
+}
---
+++ Csharp/FileOperation/Using.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Csharp.FileOperation
+{
+    internal class Using: IDisposable
+    {
+        bool disposed = false;
+        public void Dispose()
+        {
+            Dispose(true);//手動破棄
+            GC.SuppressFinalize(this);//GCに既に破棄しました、ファイナライザーの呼び出しは不要とのことです
+        }
+        protected virtual void Dispose(bool disposing)//手動破棄方法(内容は自分でカスタム)
+        {
+            if (disposed) return;
+            if (disposing)
+            {   //マネージリソースの解放
+                disposed = true;
+            }
+        }
+        ~Using()//破棄方法書き忘れた時のこちらで回収
+        {
+            Dispose(false);
+        }
+    }
+}
