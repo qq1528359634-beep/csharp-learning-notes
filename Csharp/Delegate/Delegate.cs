8c4a2da4f434c26e8a07c40501a6e298ca88440f
KANSEI CHIN <qq1528359634@gmail.com>
2026-01-18 00:37:35 +0900

    Add delegate examples and update solution files
    Added Delegate.cs and Delegate_1.cs to demonstrate delegate usage in C#. Removed ClassConcept.md and a project from the solution file. Updated Csharp.csproj to include new folders.

---
+++ Csharp/Delegate/Delegate.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Csharp.Delegate
+{　　//delegate definition
+    //delegate 戻り値の型　デリゲート名（引数の型　引数、）　　　
+    delegate void Method(string name);
+    //定義されたデリケートと同じ型な方法だけが割り当て可能
+  
+
+    internal class Delegate
+    {
+        static void Run(string str)
+        {
+            Console.WriteLine($"{str} 走る");
+        }
+       static void Main(string[] args)
+        {
+            Method method = new Method(Run);
+            method("早く");
+        }
+    }
+   
+}
+
---
+++ Csharp/Delegate/Delegate_1.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Csharp.Delegate
+{
+    internal class Delegate_1
+    {
+        public delegate void OutputPross(string str);
+
+        public void Action(string[] str, OutputPross outputPross)
+        {
+            foreach (var item in str)
+            {
+                outputPross(item);
+            }
+
+        }
+
+        public static void Main(string[] args)
+        {
+            Delegate_1 delegate_1 = new Delegate_1();
+            Actions actions = new Actions();    
+            string[] str = { "赤神", "青神", "かきがみ" };
+            OutputPross outputPross = new OutputPross(actions.Count);
+            outputPross = new OutputPross(actions.Joint);
+            Console.WriteLine(actions.CountResult);
+            Console.WriteLine(actions.JointResult);
+        }
+
+    }
+    class Actions
+    {
+        public string JointResult { get; private set; }
+        public int CountResult { get; private set; }
+        public void Joint(string str)
+        {
+
+
+            JointResult += str;
+
+        }
+        public void Count(string str)
+        {
+
+            CountResult += str.Length;
+
+        }
+    }
+}
