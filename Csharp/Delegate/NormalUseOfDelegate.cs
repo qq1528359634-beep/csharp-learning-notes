d6ae36733022ba0c4aebb3a6b11c032f3a705d44
KANSEI CHIN <qq1528359634@gmail.com>
2026-01-19 23:52:38 +0900

    Refactor delegate usage and add product wrapping logic
    Introduced ProductFactory and WrapFactory classes to demonstrate delegate usage for product creation and logging. The Main method now wraps products using delegates and logs based on price, improving code structure and clarity.

--- Csharp/Delegate/NormalUseOfDelegate.cs
+++ Csharp/Delegate/NormalUseOfDelegate.cs
@@ -8,8 +8,8 @@ namespace Csharp.Delegate
     {
         public void Log(Produce produce)
         {
-            Console.WriteLine($"Produce name is {produce.Name}" +
-                $"Product time is{DateTime.UtcNow},Produce price is{produce.Price}");
+            Console.WriteLine($" Produce name is {produce.Name}" +
+                $"Product time is {DateTime.UtcNow},Produce price is {produce.Price}");
         }
     }
     class Produce
@@ -19,13 +19,66 @@ namespace Csharp.Delegate
     }
     class Box
     {
-        Produce produce {  get; set; }
+      internal  Produce produce {  get; set; }
     }
-     class  
+     class  ProductFactory
     {
-        
+        public Produce MakeBall()
+        {
+            Produce ball = new Produce()
+            {
+                Name = "Ball"
+                ,Price = 10
+            };
+            return ball;
+            
+        }
+        public Produce MakeToyCar()
+        {
+            Produce toyCar = new Produce()
+            {
+                Name = "ToyCar"
+                ,Price = 100
+                
+            };
+            return toyCar;  
+        }
+    }
+    class WrapFactory
+    {
+        public Box WrapProduce(Func<Produce> getProduce,Action<Produce> LogCallBack)
+        {
+            Produce produce = getProduce();
+            if (produce is Produce && produce.Price > 50)
+            {
+                LogCallBack(produce);
+            }
+            Box box = new Box();
+            box.produce = produce;
+            return box;
+        }
+
     }
     internal class NormalUseOfDelegate
-    {
+    { 
+        static void Main(string[] args)
+        { 
+            ProductFactory proFactory = new();
+            Logger logger = new Logger();
+            Box box = new();
+            Box box1 = new();
+            Func<Produce> getBall=proFactory.MakeBall;
+            Func<Produce> getToyCar=proFactory.MakeToyCar;
+            Action<Produce>  logCallBack = logger.Log;
+            WrapFactory wrapFactory = new WrapFactory();
+            box = wrapFactory.WrapProduce(getToyCar, logCallBack);
+            box1 = wrapFactory.WrapProduce(getBall, logCallBack);
+            Console.WriteLine(box.produce.Name);
+            Console.WriteLine(box.produce.Price);
+            Console.WriteLine();
+            Console.WriteLine();
+            Console.WriteLine(box1.produce.Name);
+            Console.WriteLine(box1.produce.Price);
+        }
     }
 }
