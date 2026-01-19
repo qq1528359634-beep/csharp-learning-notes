c335c2fb0fe618f4a58c1c53d0c7820f9371ffe7
KANSEI CHIN <qq1528359634@gmail.com>
2026-01-19 23:26:11 +0900

    Add examples of delegate usage in C#
    Added NormalUseOfDelegate.cs and SimpleUseOfDelegate.cs demonstrating basic and practical usage of delegates in C#. Includes sample classes for logging, calculation, and delegate assignment.

---
+++ Csharp/Delegate/NormalUseOfDelegate.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Csharp.Delegate
+{
+    class Logger
+    {
+        public void Log(Produce produce)
+        {
+            Console.WriteLine($"Produce name is {produce.Name}" +
+                $"Product time is{DateTime.UtcNow},Produce price is{produce.Price}");
+        }
+    }
+    class Produce
+    {
+        public string Name { get; set; }    
+        public double Price { get; set; }   
+    }
+    class Box
+    {
+        Produce produce {  get; set; }
+    }
+     class  
+    {
+        
+    }
+    internal class NormalUseOfDelegate
+    {
+    }
+}
---
+++ Csharp/Delegate/SimpleUseOfDelegate.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Csharp.Delegate
+{
+    class Calculator
+    {
+        public int Add(int a, int b)
+        {
+            return a + b;
+        }
+        public int Sub(int a, int b)
+        {
+            return a - b;
+        }
+        public int OutOnenumber()
+        {
+            return 5;
+        }
+        public void Print()
+        {
+            Console.WriteLine("I have five methods");
+        }
+        public int Get(int a)
+        {
+            return a;
+        }
+
+    }
+    internal class SimpleUseOfDelegate
+    {
+        static void Main(string[] args)
+        {
+            Calculator cal = new Calculator();
+            Action action = cal.Print;
+            action();
+            Func<int> func = cal.OutOnenumber;
+            Console.WriteLine(func());//return 5
+            Func<int, int, int> func1 = cal.Add;
+            int x = 100, y = 200, z;
+            z = func1(x, y);
+            Console.WriteLine(z);//300
+            func1 = cal.Sub;
+            z = func1(x, y);
+            Console.WriteLine(z);//-100
+            Type type = func1.GetType();
+            Console.WriteLine(type.Name);//Func 3
+            Console.WriteLine(type.IsClass);//true
+            Func<int, int> func2 = cal.Get;
+            type = func2.GetType();
+            Console.WriteLine(type.Name);//Func 2
+
+        }
+    }
+}
