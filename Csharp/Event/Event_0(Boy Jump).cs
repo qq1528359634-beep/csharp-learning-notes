eaa91cad72a8bf8f4d23c4acf8e740183464b3a1
KANSEI CHIN <qq1528359634@gmail.com>
2026-01-20 23:36:52 +0900

    Add event handling example with Timer, Boy, and Girl
    Introduces a new C# example demonstrating event subscription using System.Timers.Timer. The example includes Boy and Girl classes responding to timer events with different actions.

---
+++ Csharp/Event/Event_0(Boy Jump).cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Timers;
+
+namespace Csharp.Event
+{
+    internal class Event_0
+    {
+        static void Main(string[] args)
+        {   //event Source 事件的拥有者
+            System.Timers.Timer timer = new();
+            timer.Interval = 1000;
+            //事件的响应者
+            Boy boy = new Boy();
+            Girl girl = new Girl();
+            //事件  订阅   事件处理器
+            timer.Elapsed += boy.Action;
+            timer.Elapsed += girl.Action;
+            timer.Start();
+            Console.ReadLine();
+
+        }
+    }
+    class Boy
+    {
+        internal void Action(object? sender, ElapsedEventArgs e)
+        {
+            Console.WriteLine("Jump !");
+        }
+    }
+    class Girl
+    {
+        internal void Action(object? sender, ElapsedEventArgs e)
+        {
+            Console.WriteLine("Sing !");
+        }
+    }
+
+}
+}
