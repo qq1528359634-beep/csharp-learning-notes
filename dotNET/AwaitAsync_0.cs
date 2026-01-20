5f223e0babfcf6af8d23936c7fea50e018720b85
KANSEI CHIN <qq1528359634@gmail.com>
2026-01-20 22:53:11 +0900

    Add async file read/write example in C# project
    Introduced a new C# console application demonstrating asynchronous file operations using await. Added dotNET.csproj for project configuration targeting .NET 10.0.

---
+++ dotNET/AwaitAsync_0.cs
@@ -0,0 +1,31 @@
+﻿
+using System.Text;
+
+namespace AwaitAsync1
+{
+
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            /*string filename = @"C:\Users\15283\source\repos\Drafts\temp\1.txt";
+            File.WriteAllText(filename, "hello");
+            string s = File.ReadAllText(filename);
+            Console.WriteLine(s);*/
+            string filename = @"C:\Users\15283\source\repos\Drafts\temp\1.txt";
+            StringBuilder sb = new StringBuilder();
+            for (int i = 0; i < 10000; i++)
+            {
+                sb.AppendLine("hello");
+            }
+            //如果没有await则下个文件不等他写完就访问 同时2个进程访问一个文件会报错
+            await File.WriteAllTextAsync(filename, sb.ToString());
+            string s = await File.ReadAllTextAsync(filename);
+            Console.WriteLine(s);
+            Task<string> t = File.ReadAllTextAsync(filename);
+            string a = await t;
+        }
+    }
+
+
+}
