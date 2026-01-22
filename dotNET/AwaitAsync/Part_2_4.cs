e6fb871077ba786ab70b657047cc2784c6b58540
KANSEI CHIN <qq1528359634@gmail.com>
2026-01-22 23:18:49 +0900

    Revert "Update Event_0(Boy Jump).cs"
    This reverts commit 8e72b96000d57468c91763a2a255adf34a7aab2d.

---
+++ dotNET/AwaitAsync/Part_2_4.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace dotNET.AwaitAsync
+{
+    internal class Part_2_4
+    {
+        async Task Main(string[] args)
+        {
+            // string filename = @"C:\Users\15283\source\repos\file.txt";
+            // File.WriteAllText(filename,"Hello World !");
+            //string str= File.ReadAllText(filename);
+            // Console.WriteLine(str);
+            string filename = @"C:\Users\15283\source\repos\file.txt";
+            await File.WriteAllTextAsync(filename, "Hello World");
+            StringBuilder sb = new StringBuilder();
+            for (int i = 0; i < 100000; i++)
+            {
+                sb.AppendLine("Hello");
+            }
+            await File.WriteAllTextAsync(filename, sb.ToString());
+            string str = await File.ReadAllTextAsync(filename);
+            //Task<string> str =  File.ReadAllTextAsync(filename);
+            //string str2 = await str;
+            Console.WriteLine();
+        }
+    }
+}
---
+++ dotNET/AwaitAsync/Part_2_5.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace dotNET.AwaitAsync
+{
+    internal class Part_2_5
+    {
+        async Task Main_(string[] args)
+        {
+            string url = "https://www.youzack.com";
+            string deskFilePath = @"C:\Users\15283\source\repos\filename.txt";
+            var numb = await DownloadsWebAsync(url, deskFilePath);
+            Console.WriteLine(numb);
+
+
+        }
+        //假如没有异步方法可以调用
+        void Main_()
+        {
+            string deskFilePath = @"C:\Users\15283\source\repos\filename.txt";
+            Task<string> t = File.ReadAllTextAsync(deskFilePath);
+            //Result 属性でｔの値を取得する
+            string str = t.Result;
+            Console.WriteLine(str);
+            File.WriteAllTextAsync(@"C:\Users\15283\source\repos\1.txt", "str").Wait();
+
+        }
+
+        static async Task<int> DownloadsWebAsync(string url, string destFilePath)
+        {
+
+            string body = "";
+            using (HttpClient client = new HttpClient())
+            { //HttpClient にはIDisposableが付いてる　using でリソース手動回収
+                string str = await client.GetStringAsync(url);
+                await File.WriteAllTextAsync(destFilePath, str);
+
+                Console.WriteLine(str);
+                Console.WriteLine("OK");
+
+            }
+            return body.Length;
+        }
+        static void Main(string[] args)
+        {
+            ThreadPool.QueueUserWorkItem(async (obj) =>
+            {
+                while (true)
+                {
+                    await File.WriteAllTextAsync(@"C:\Users\15283\source\repos\2.txt", "aaaa");
+                }
+            });
+            Console.Read();
+        }
+    }
+}
