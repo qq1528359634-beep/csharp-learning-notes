5ea24d883df2d6108d8d29af4deec68ab127f08c
KANSEI CHIN <qq1528359634@gmail.com>
2026-01-26 22:27:55 +0900

    Update Part_2_14_HowLINQWork.cs

--- dotNET/LINQ/Part_2_14_HowLINQWork.cs
+++ dotNET/LINQ/Part_2_14_HowLINQWork.cs
@@ -1,7 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
-
+using System.Linq;
 namespace dotNET.LINQ
 {
     #region Why  LINQ
@@ -9,5 +9,48 @@ namespace dotNET.LINQ
     #endregion
     internal class Part_2_14_HowLINQWork
     {
+        static void Main(string[] args)
+        {   //using System.Linq
+            int[] nums = new int[] { 3, 5, 66, 77, 33, 2, 9, 35 };
+            //where 方法遍历集合中的每个元素对于集合中的每个元素都调用
+            //a=>a>10 判断是否为true 如果为true 则返回集合之中
+            // IEnumerable<int> reslut = nums.Where(a => a > 10);
+            // IEnumerable<int> reslut = MyWhere2(nums, a => a > 10);
+            var reslut = MyWhere2(nums, a => a > 10);
+            foreach (var item in reslut)
+            {
+                Console.WriteLine(item);
+            }
+        }
+
+        static IEnumerable<int> MyWhere1(IEnumerable<int> items, Func<int, bool> f)
+        {
+            List<int> result = new List<int>();
+            foreach (var item in items)
+            {
+                if (f(item) == true)
+                {
+                    Console.WriteLine("MyWhere2:" + item);
+                    result.Add(item);
+                }
+            }
+            return result;
+        }
+
+        //yield return
+        static IEnumerable<int> MyWhere2(IEnumerable<int> items, Func<int, bool> f)
+        {
+            foreach (var item in items)
+            {
+                if (f(item) == true)
+                {
+                    Console.WriteLine("MyWhere2:" + item);
+                    yield return item;
+                }
+
+            }
+
+
+        }
     }
 }
