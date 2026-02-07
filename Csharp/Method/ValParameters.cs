0b859fc0604d7f82b4e0769f1123eb3d1d4be327
KANSEI CHIN <qq1528359634@gmail.com>
2026-02-07 11:15:17 +0900

    add ValParameters demo

---
+++ Csharp/Method/ValParameters.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Csharp.Method
+{
+    internal class ValParameters
+    {
+        static void Main(string[] args)
+        {
+            int val = 10;
+
+            ClassParameter cValue = new ClassParameter();
+            cValue.Value = 10;
+
+            AddMethod(val, cValue);
+            Console.WriteLine($"the value of cValue is {cValue.Value}\n" +
+                $"the value of val is {val}");
+            //the value of cValue is 20
+           // the value of val is 10
+        }
+        static void AddMethod(int valueParameter, ClassParameter referenceParameter)
+        {
+            valueParameter += 10;
+            referenceParameter.Value += 10;
+        }
+    }
+    class ClassParameter
+    {
+        public int Value;
+    }
+}
