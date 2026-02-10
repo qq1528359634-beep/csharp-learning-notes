ceaf0079740614bede1177ca1703382271ddf9fc
KANSEI CHIN <qq1528359634@gmail.com>
2026-02-10 22:45:20 +0900

    Create implicit_explicit.cs

---
+++ Csharp/Conversion/implicit_explicit.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Csharp.Conversion
+{
+    internal class implicit_explicit
+    {
+    }
+    class Person
+    {
+        public int Age { get; set; }
+        public string Name { get; set; }
+        public Person(string name, int age)
+        {
+            Age = age;
+            Name = name;
+        }
+        public static explicit operator Person(int age)
+        {
+            return new Person("Somebdoy",age);
+        }
+        public static implicit operator int(Person person)
+        {
+            return person.Age;
+        }
+    }
+    
+    }
