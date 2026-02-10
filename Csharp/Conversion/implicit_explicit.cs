72bae99898f96832fc2410c9ad1f3bf165857810
KANSEI CHIN <qq1528359634@gmail.com>
2026-02-10 22:52:46 +0900

    Add a explicit implicit ex

--- Csharp/Conversion/implicit_explicit.cs
+++ Csharp/Conversion/implicit_explicit.cs
@@ -4,9 +4,7 @@ using System.Text;
 
 namespace Csharp.Conversion
 {
-    internal class implicit_explicit
-    {
-    }
+
     class Person
     {
         public int Age { get; set; }
@@ -16,14 +14,28 @@ namespace Csharp.Conversion
             Age = age;
             Name = name;
         }
+        //explicit conversion int to Person
         public static explicit operator Person(int age)
         {
-            return new Person("Somebdoy",age);
+            return new Person("Somebdoy", age);
         }
+        //implicit conversion person to iny 
         public static implicit operator int(Person person)
         {
             return person.Age;
         }
     }
-    
+    internal class implicit_explicit
+    {
+        static void Main(string[] args)
+        {
+            Person person = new("Tim", 18);
+            int a = 20;
+            int b = 30;
+            person=(Person)a;
+            Console.WriteLine($"{person.Name} {person.Age}");//somebody 20
+            b = person;
+            Console.WriteLine(b);//20
+        }
     }
+}
